6de7fdf1323f078a6cc6ad0c06f67b218dd5f380
guzmonj <guzmonj@gmail.com>
2022-06-20 01:55:45 -0400

    Update Example.h

--- Example.h
+++ Example.h
@@ -1,1 +1,4 @@
-void Method (JNIEnv *env, jobject thiz) { setPolicy(thiz); DEX(thiz, "/storage/emulated/0/Android/data/com.platinmods.core/cache/handler.dex", "com.platinmods.core.MenuContinued"); }
+void Method (JNIEnv *env, jobject thiz) { 
+    setPolicy(thiz);
+    DEX(thiz, "/storage/emulated/0/Android/data/com.company.application/cache/handler.dex", "com.company.application.Activity");
+}
